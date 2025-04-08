b3d8fc13858f8d8c07b3d1b813d1d4881dee2b28
Ha Bao <baokutodz@gmail.com>
2025-04-08 19:50:16 +0700

    first commit

---
+++ src/components/home/home.tsx
@@ -0,0 +1,28 @@
+export function Home() {
+  return (
+    <nav className="padding-x py-8 absolute z-10 w-full">
+      <ul className="font-mono mt-5 flex items-center justify-center gap-x-20 text-white text-base">
+        <li>
+          <a href="#" className="">
+            HOME
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            ABOUT
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            PRODUCT
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            CONTACT
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}

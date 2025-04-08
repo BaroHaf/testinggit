e953d39f7b09319478d3d012784042cd9616eca2
Ha Bao <hagiabao13102004@gmail.com>
2025-04-08 20:27:04 +0700

    third change

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -9,7 +9,7 @@ export function Home() {
         </li>
         <li>
           <a href="#" className="">
-            ABOUT CON CACAC KI LON
+            ABOUT Co cai lon nhe con
           </a>
         </li>
         <li>
